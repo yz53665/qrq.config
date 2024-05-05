354a4fcd53178d01f4894da2871b56582ab5cbab
qiuruiqi <qiuruiqi@xiaomi.com>
2024-05-05 11:36:39 +0800

    精简配置文件，以python语言为主

--- .custom.el
+++ .custom.el
@@ -1,18 +1,7 @@
 (add-to-list 'melpa-include-packages 'org-download)
 (add-to-list 'melpa-include-packages 'dap-mode)
-(add-to-list 'load-path "/mnt/d/myfile/capture/qrq.config")
-(cond
- ((eq system-type 'gnu/linux)
-  (require 'qrq-linux))
- ((eq system-type 'darwin)
-  (require 'qrq-mac)))
-
-;;   ;; Don't ask before reloading updated tags files
-  (setq tags-revert-without-query t)
-;;   ;; NO warning when loading large tag files
-  (setq large-file-warning-threshold nil)
-
 ;;============================org-mode configuration========================
+(setq-default qrq/org-basic-directory "~/org")
 (setq-default org-directory qrq/org-basic-directory)
 (setq-default org-agenda-files (list org-directory))
 
@@ -126,18 +115,12 @@
         ("org-blog"
          :components ("org-to-blog-html" "other-static-file"))
         ))
-;;=============================================org configuration end==========================================
-;;=============================zotxt=====================================
-(require-package 'zotxt)
-;;=============================you-dao package===========================
-(require-package 'youdao-dictionary)
-;;============================youdao package end=========================
-
 (defun org-dblock-write:block-update-time (params)
   (let ((fmt (or (plist-get params :format) "%d. %m. %Y")))
     (insert "Last block update at: "
             (format-time-string fmt (current-time)))))
 
+;;=============================================org configuration end==========================================
 ;;=========================================org-download configuration==================
 (require-package 'org-download)
 (add-hook 'dired-mode-hook 'org-download-enable)
@@ -149,46 +132,40 @@
 ;;=======================================org-download configuration end===============
 
 ;;==========================================lsp configuration============================
-(require-package 'lsp-mode)
-;;{{ lsp-mode configuration
 (with-eval-after-load 'lsp-mode
   ;; enable log only for debug
-  (setq-default lsp-log-io nil)
+  (setq lsp-log-io nil)
   ;; use `evil-matchit' instead
-  (setq-default lsp-enable-folding nil)
+  (setq lsp-enable-folding nil)
   ;; no real time syntax check
-  (setq-default lsp-diagnostic-package :none)
+  (setq lsp-diagnostic-package :none)
   ;; handle yasnippet by myself
-  (setq-default lsp-enable-snippet nil)
-  ;; use `company-ctags' only.
-  ;; Please note `company-lsp' is automatically enabled if it's installed
-  (setq-default lsp-enable-completion-at-point nil)
+  (setq lsp-enable-snippet nil)
   ;; turn off for better performance
-  (setq-default lsp-enable-symbol-highlighting nil)
+  (setq lsp-enable-symbol-highlighting nil)
   ;; use find-fine-in-project instead
-  (setq-default lsp-enable-links nil)
+  (setq lsp-enable-links nil)
   ;; auto restart lsp
-  (setq-default lsp-restart 'auto-restart)
+  (setq lsp-restart 'auto-restart)
+  ;;pylsp configuration
+  (setq lsp-completion-enable nil)
   ;; don't watch 3rd party javascript libraries
   (push "[/\\\\][^/\\\\]*\\.\\(json\\|html\\|jade\\)$" lsp-file-watch-ignored)
   ;; don't ping LSP language server too frequently
-  (setq-default lsp-ui-sideline-enable nil)
-  (setq-default lsp-ui-sideline-show-code-actions nil)
-  (setq-default lsp-ui-sideline-show-hover nil)
-  (setq-default lsp-modeline-code-actions-enable t)
-  (setq-default lsp-eldoc-enable-hover nil)
-  (setq-default lsp-signature-auto-activate nil)
-  )
+  (defvar lsp-on-touch-time 0)
+  (defun my-lsp-on-change-hack (orig-fun &rest args)
+    ;; do NOT run `lsp-on-change' too frequently
+    (when (> (- (float-time (current-time))
+                lsp-on-touch-time) 20) ;; 2 mins
+      (setq lsp-on-touch-time (float-time (current-time)))
+      (apply orig-fun args)))
+  (advice-add 'lsp-on-change :around #'my-lsp-on-change-hack))
 
-(evil-define-key '(normal visual) 'lsp
-  (kbd "C-]") 'xref-find-definitions
-  (kbd "C-'") 'xref-find-references)
 
 (evil-define-key '(normal visual) 'c-mode
   (kbd "C-]") 'counsel-etags-find-tag-at-point
   (kbd "C-'") 'counsel-etags-find-tag-at-point)
 
-;; (add-hook 'c++-mode-hook #'lsp-deferred)
 
 ;;=======================================c-mode configuration==========================
 (add-hook 'c-mode-hook #'lsp-deferred)
@@ -209,69 +186,59 @@
 ;;=======================================lsp configuration end============================
 
 (setq lazyflymake-flymake-mode-on t)
-;; flycheck
-;; (require-package 'flycheck)
-;; (add-hook 'after-init-hook #'global-flycheck-mode)
-;; ;; disable default lazyflymake to use flycheck
-;; (setq-default my-disable-lazyflymake t)
 
 ;;===================================go-mode=============================================
-(require-package 'go-mode)
+;; (require-package 'go-mode)
 
-(add-hook 'go-mode-hook #'lsp-deferred)
+;; (add-hook 'go-mode-hook #'lsp-deferred)
 
-(with-eval-after-load 'go-mode
-  (setq-default gofmt-command "goimports")
-  (setq-default lsp-gopls-use-placeholders t)
-  (evil-define-key '(normal visual) go-mode-map (kbd "C-]") 'godef-jump)
-  (evil-define-key '(normal visual) go-mode-map (kbd "C-o") 'xref-pop-marker-stack)
-  )
+;; (with-eval-after-load 'go-mode
+;;   (setq-default gofmt-command "goimports")
+;;   (setq-default lsp-gopls-use-placeholders t)
+;;   (evil-define-key '(normal visual) go-mode-map (kbd "C-]") 'godef-jump)
+;;   (evil-define-key '(normal visual) go-mode-map (kbd "C-o") 'xref-pop-marker-stack)
+;;   )
 
-
-;; (add-to-list 'load-path "folder-in-which-go-dlv-files-are-in/") ;; if the files are not already in the load path
-;; (require-package 'go-dlv)
 ;;=================================go-mode configuration end==============================
 
 ;;=================================dap-mode configuration================================
+(require-package 'lsp-docker)
 (require-package 'dap-mode)
-(require 'dap-go)
-(require 'dap-lldb)
-(require 'dap-python)
-(require 'dap-gdb-lldb)
-(dap-go-setup)
-
+(add-hook 'dap-stopped-hook
+          (lambda (arg) (call-interactively #'dap-hydra)))
 
 (with-eval-after-load 'dap-mode
-  ;; unable to use
-  ;;(setq-default dap-lldb-debug-program '("/usr/local/Cellar/llvm/12.0.1/bin/lldb-vscode"))
   (setq-default dap-auto-configure-features '(sessions locals controls tooltip))
   (custom-set-faces
    '(dap-ui-pending-breakpoint-face ((t (:underline "dim gray"))))
    '(dap-ui-verified-breakpoint-face ((t (:underline "green")))))
   )
-(dap-mode 1)
-(dap-ui-mode 1)
+
+(require 'dap-python)
+;; if you installed debugpy, you need to set this
+;; https://github.com/emacs-lsp/dap-mode/issues/306
+(setq dap-python-debugger 'debugpy)
 
 ;;=================================dap-mode end==========================================
 
 ;;================================csharp-mode configuration==============================
-(require-package 'csharp-mode)
-(require-package 'omnisharp)
+;; (require-package 'csharp-mode)
+;; (require-package 'omnisharp)
 
-(with-eval-after-load 'csharp-mode
+;; (with-eval-after-load 'csharp-mode
 
-  (evil-define-key '(normal visual) csharp-mode-map (kbd "C-]") 'xref-find-definitions)
-  (evil-define-key '(normal visual) csharp-mode-map (kbd "C-'") 'xref-find-references)
-  )
+;;   (evil-define-key '(normal visual) csharp-mode-map (kbd "C-]") 'xref-find-definitions)
+;;   (evil-define-key '(normal visual) csharp-mode-map (kbd "C-'") 'xref-find-references)
+;;   )
 
-(defun my-csharp-mode-hook ()
-  ;; enable the stuff you want for C# here
-  (electric-pair-mode 1)       ;; Emacs 24
-  (electric-pair-local-mode 1) ;; Emacs 25
-  (setq imenu-create-index-function 'counsel-etags-imenu-default-create-index-function)
-  )
-(add-hook 'csharp-mode-hook 'lsp-deferred)
-(add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
+;; (defun my-csharp-mode-hook ()
+;;   ;; enable the stuff you want for C# here
+;;   (electric-pair-mode 1)       ;; Emacs 24
+;;   (electric-pair-local-mode 1) ;; Emacs 25
+;;   (setq imenu-create-index-function 'counsel-etags-imenu-default-create-index-function)
+;;   )
+;; (add-hook 'csharp-mode-hook 'lsp-deferred)
+;; (add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
 ;;===============================csharp-mode configuration end==========================
 
 ;;;; set gdb multi-windows when open
@@ -279,21 +246,8 @@
 
 ;;emacs theme configuration
 (load-theme 'sanityinc-tomorrow-eighties t)
-;;(load-theme 'solarized-dark t)
 
-;;===============================verlog-mode configuration==============================
-(require 'lsp-verilog)
-
-(with-eval-after-load 'verilog-mode
-  (custom-set-variables
-   '(lsp-clients-svlangserver-launchConfiguration "/tools/verilator -sv --lint-only -Wall")
-   '(lsp-clients-svlangserver-formatCommand "/tools/verible-verilog-format"))
-  )
-
-(add-hook 'verilog-mode-hook #'lsp-deferred)
-;;==============================verlog-mode configuration end==========================
-
-;;==============================elpy configuration=====================================
+;;==============================python configuration=====================================
 (set-fringe-style (quote (nil . nil)))
 (custom-set-faces
  '(dap-ui-pending-breakpoint-face ((t (:underline "dim gray"))))
@@ -302,21 +256,14 @@
 (setq left-fringe-width 12)
 
 (with-eval-after-load 'elpy
-  (setq-default elpy-rpc-virtualenv-path (concat (getenv "WORKON_HOME") "rpc-venv/"))
-  (let ((venv-dir (concat (getenv "WORKON_HOME") "cv/")))
-    (if (file-exists-p venv-dir) (pyvenv-activate venv-dir)))
+  (let* ((venv-dir "~/miniconda3/envs/mm"))
+    (when (and (file-exists-p venv-dir)
+               (executable-find pyvenv-virtualenvwrapper-python))
+      (pyvenv-activate venv-dir))))
 
-
-  ;; (setq python-shell-interpreter "python"
-  ;;       python-shell-interpreter-args "-i")
-  )
-
+(add-hook 'python-mode-hook 'anaconda-mode)
 ;; 运行时让每个python的buffer都用单独的shell,避免共享变量,造成冲突
 (add-hook 'elpy-mode-hook (lambda () (elpy-shell-toggle-dedicated-shell 1)))
-
-;; 每个project一个shell
-;;(add-hook 'elpy-mode-hook (lambda () (elpy-shell-set-local-shell (elpy-project-root))))
-;; format-code
 
 ;;=============================elpy configuration end===================================
 
@@ -362,9 +309,6 @@
     "oa" 'org-agenda
     "oc" 'org-capture
     "os" 'org-save-all-org-buffers
-    ;;}}
-    "yda" 'youdao-dictionary-search-at-point
-    "ydi" 'youdao-dictionary-search-from-input
     ;;{{ my func
     "qnl" 'qrq/auto-newline
     "qap" 'qrq/add-prefix-for-multiple-line
@@ -395,44 +339,6 @@
 (defhydra hydra-eaf ()
   "
 ")
-
-;;==============================w3m========================================
-(setq hostip (concat "http_proxy=http://" hostip ":7890"))
-(setq w3m-command-arguments-alist
-      '(
-	;; Use the proxy server to visit any foreign urls.
-	("-o" hostip)))
-;;==============================w3m end===================================
-
-;;========================================keyfreq configuration============================================
-;; (require 'keyfreq)
-;; (keyfreq-mode 0)
-;; (keyfreq-autosave-mode 0)
-;; (setq keyfreq-excluded-commands
-;;       '(
-;;         org-self-insert-command
-;;         self-insert-command
-;;         dap-tooltip-mouse-motion
-;;         newline-and-indent
-;;         ivy-next-line
-;;         ivy-previous-line
-;;         ivy-backward-delete-char
-;;         ivy-done
-;;         company-select-next-or-abort
-;;         company-complete-selection
-;;         mouse-set-region
-;;         evil-mouse-drag-region
-;;         undefined
-;;         ignore
-;;         evil-insert
-;;         evil-next-line
-;;         evil-previous-line
-;;         evil-forward-char
-;;         evil-backward-char))
-;;========================================keyfreq configuration end========================================
-
-(setq my-term-program "/bin/zsh")
 (exec-path-from-shell-initialize)
 
-(provide '.custom)
-;;
+;; (provide '.custom)
